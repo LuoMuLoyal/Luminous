8046b0c4eedf60f93ba38f04373df17130af430b
Luo <2508015296@qq.com>
2026-08-29 19:49:05 +0800

    fix(shell): 去掉 Column mainAxisSize.min 以消除与 Expanded 的约束冲突

--- lib/features/shell/presentation/page.dart
+++ lib/features/shell/presentation/page.dart
@@ -67,7 +67,6 @@ class ShellPage extends ConsumerWidget {
                 onSelectTab: onSelectTab,
               ),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
             const ConnectivityBanner(),
             Expanded(child: content),
